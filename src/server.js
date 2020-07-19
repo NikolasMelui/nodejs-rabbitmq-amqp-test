66425c1f3606d4c873eb607cc4f0cd18494b174b
nikolasmelui <slak@samaradom.ru>
2020-07-19 15:59:56 +0400

    Add simple logic to publish the messages to the queue

--- src/server.js
+++ src/server.js
@@ -4,6 +4,26 @@ const http = require('http');
 const { APPLICATION_HOST, APPLICATION_PORT } = require('./config');
 const { getApplicationHost, getApplicationPort } = require('./helpers');
 
+const amqp = require('amqplib');
+
+(async () => {
+  try {
+    const connection = await amqp.connect('amqp://localhost:5672');
+    const channel = await connection.createChannel();
+    const assertQueueResult = await channel.assertQueue('jobs');
+    console.log(`Assert queue results: ${assertQueueResult}`);
+    const sendToQueueResult = channel.sendToQueue(
+      'jobs',
+      Buffer.from(
+        JSON.stringify({ login: 'nikolasmelui', password: 'password' }),
+      ),
+    );
+    console.log(`Send to queue results: ${sendToQueueResult}`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 http
   .createServer((req, res) => {
     res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
